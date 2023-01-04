1562076c855c8c8a5cc0c0b06d504cbf0131f118
Paula <paulakaferreira@gmail.com>
2023-01-04 15:43:46 +0100

    Create Square structure
    The board is now a two dimensional array of squares

--- board.c
+++ board.c
@@ -2,38 +2,57 @@
 
 #define BOARD_SIZE 15
 
-char board_value[BOARD_SIZE][BOARD_SIZE];
+struct Square 
+{
+    int value;
+    char type;
+    char tile;
+
+};
+
+struct Square board[BOARD_SIZE][BOARD_SIZE];
 char jeton;
 int column, row;
 int i = 0;
 int j = 0;
 
 void create_board();
+void get_position();
 void print_board();
-void modify_board(char jeton, int column, int row);
+void modify_board();
+void set_board_value();
 
 int main()
 {
     create_board();
     print_board();
-    printf("Enter letter: ");
-    scanf("%c", &jeton);
-    printf("Enter column ");
-    scanf("%d", &column);
-    printf("Enter row: ");
-    scanf("%d", &row);
-    modify_board(jeton, column, row);
+    get_position();
+    modify_board();
     print_board();
     return 0;
 }
 
+void get_position()
+{
+
+    printf("Entrez la lettre à ajouter au tableau: ");
+    scanf("%c", &jeton);
+    printf("Entrez le numéro de la colomne: ");
+    scanf("%d", &column);
+    printf("Entrez le numéro de la ligne: ");
+    scanf("%d", &row);
+
+}
+
 void create_board()
 {
+    // i pour la ligne
     for (i = 0; i < BOARD_SIZE; i++)
     {
+        // j pour la colomne
         for (j = 0; j < BOARD_SIZE; j++)
         {
-            board_value[i][j] = '1';
+            board[i][j].tile = '0';
         }
     }
 }
@@ -45,7 +64,7 @@ void print_board()
     {
         for (j = 0; j < BOARD_SIZE; j++)
         {            
-            printf("%c ", board_value[i][j]);
+            printf("%c ", board[i][j].tile);
             if (j == BOARD_SIZE - 1)
             {
                 printf("\n");
@@ -55,9 +74,32 @@ void print_board()
 
 }
 
-void modify_board(char jeton, int column, int row)
+void set_board_value()
+{
+
+    for (i = 0; i < BOARD_SIZE; i++)
+    {
+        for (j = 0; j < BOARD_SIZE; j++)
+        {
+            if ((i == 0 && j == 0) || (i == 0 && j == 7) || (i == 0 && j == 14))
+            {
+                board[i][j].value = 3;
+                // Le type 'M' indique la valeur totale du mot 
+                board[i][j].type = 'M';
+            }
+            else
+            {
+                board[i][j].value = 1;
+                board[i][j].type = NULL;
+            }
+        }
+    }
+
+}
+
+void modify_board()
 {
     column--;
     row--;
-    board_value[column][row] = jeton;
+    board[row][column].tile = jeton;
 }
