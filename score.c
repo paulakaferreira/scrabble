dc659186e490b00c28f88c9e631b6e94940177c0
Paula <paulakaferreira@gmail.com>
2023-01-20 23:40:10 +0100

    Create score.c

---
+++ score.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "board.c"
+#include "player.c"
+
+int multiply(int letter_value, int tiles_count, int square_value, char square_flag)
+{
+    int sum = 0;
+    if (square_flag == 'W')
+    {
+
+    }
+    else if (square_flag == 'L')
+    {
+
+    }
+    else
+    {
+        
+    }
+    return sum;
+}
